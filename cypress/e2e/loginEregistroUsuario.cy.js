bd8432578190a9f0796e24bbfc93642a119e404a
Danielle Silveira Schaidhauer <118474401+danischaidhauer@users.noreply.github.com>
2024-06-22 17:34:27 +0100

    ajuste

--- cypress/e2e/loginEregistroUsuario.cy.js
+++ cypress/e2e/loginEregistroUsuario.cy.js
@@ -49,7 +49,7 @@ describe('Acesso a página do bugbank', () => {
         cy.get('p[id="modalText"]').contains('As senhas não são iguais.').should('be.visible');
     }); 
 
-    it.only('CT05 - Tentativa de Cadastro com e-mail inválido', () => {
+    it('CT05 - Tentativa de Cadastro com e-mail inválido', () => {
         cy.get('.ihdmxA').click();
         cy.wait(1000);
         cy.get('input[placeholder="Informe seu e-mail"]').eq(1).type('sadsadasdasd', { force: true });
@@ -76,10 +76,23 @@ describe('Acesso a página do bugbank', () => {
         cy.get('#btnCloseModal').click();
     })
 
-    it('CT08 - Login realizado com sucesso, preenchendo todos os campos obrigatórios', () => {
+    it.only('CT08 - Login realizado com sucesso, preenchendo todos os campos obrigatórios', () => {
+        cy.get('.ihdmxA').click();
+        cy.wait(1000);
+        cy.get('input[placeholder="Informe seu e-mail"]').eq(1).type(credentials.email, { force: true });
+        cy.get('input[placeholder="Informe seu Nome"]').type('Danielle', { force: true });
+        cy.wait(1000);
+        cy.get(':nth-child(4) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type(credentials.password, { force: true });        
+        cy.get(':nth-child(5) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type(credentials.password, { force: true });
+        cy.get('#toggleAddBalance').click({ force: true });
+        cy.get('button[class="style__ContainerButton-sc-1wsixal-0 CMabB button__child"]').click({ force: true });
+        cy.get('.styles__ContainerContent-sc-8zteav-1').should('exist');
+        cy.get('#btnCloseModal').click();
         cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > :nth-child(1) > .input__default').type(credentials.email);
-        cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > .login__password > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type(credentials.password)
+        cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > .login__password > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type(credentials.password);
         cy.get('.otUnI').click();
         cy.url().should('eq', 'https://bugbank.netlify.app/home');
+
+
     });
 });
---
+++ cypress/e2e/transferenciasBancarias.cy.js
@@ -0,0 +1,49 @@
+require('cypress-xpath');
+
+describe('Acesso a página do bugbank', () => {
+    const credentials = require('../fixtures/credentials.json');
+
+    // Antes de todos os testes, realizar o login válido
+    before(() => {
+        cy.visit('/');
+        cy.wait(2000);
+
+        // criar usuario e logar
+        cy.get('.ihdmxA').click();
+        cy.wait(1000);
+        cy.get('input[placeholder="Informe seu e-mail"]').eq(1).type(credentials.email, { force: true });
+        cy.get('input[placeholder="Informe seu Nome"]').type('Danielle', { force: true });
+        cy.wait(1000);
+        cy.get(':nth-child(4) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type(credentials.password, { force: true });        
+        cy.get(':nth-child(5) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type(credentials.password, { force: true });
+        cy.get('#toggleAddBalance').click({ force: true });
+        cy.get('button[class="style__ContainerButton-sc-1wsixal-0 CMabB button__child"]').click({ force: true });
+        cy.get('.styles__ContainerContent-sc-8zteav-1').should('exist');
+        cy.get('#btnCloseModal').click();
+        cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > :nth-child(1) > .input__default').type(credentials.email);
+        cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > .login__password > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type(credentials.password);
+        cy.get('.otUnI').click();
+        cy.url().should('eq', 'https://bugbank.netlify.app/home');
+    });
+
+    // Testes relacionados ao login e operações bancárias
+    it('CT06 - Tentativa de Transferência para conta inválida', () => {
+        cy.get('#btn-TRANSFERÊNCIA').click();
+        cy.get(':nth-child(1) > .input__default').type('344');
+        cy.get('.account__data > :nth-child(2) > .input__default').type('0');
+        cy.get('.styles__ContainerFormTransfer-sc-1oow0wh-0 > :nth-child(2) > .input__default').type('50');
+        cy.get(':nth-child(3) > .input__default').type('despesa alimentação');
+        cy.get('.style__ContainerButton-sc-1wsixal-0').click();
+        cy.get('#modalText').contains('Conta inválida ou inexistente').should('exist');
+        cy.get('#btnCloseModal').click();
+    });
+
+    it('CT07 - Tentativa de Transferência com saldo insuficiente', () => {
+        
+    });
+
+    it('CT10 - Transferência para uma conta válida, realizada com sucesso', () => {
+        
+    });
+
+});
